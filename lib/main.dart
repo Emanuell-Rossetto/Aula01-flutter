f4982151b4e2d5b83fbc03010b96af094c9c3dd9
Emanuell-Rossetto <emanuellrossetto@hotmail.com>
2026-03-23 21:42:12 -0300

    Coluns, children, e alinhamentos

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,23 @@ void main (){
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Text("Will?")
+        appBar: AppBar(
+          title: Center(
+            child: Text("Stranger things"),
+          ),
+          backgroundColor: const Color.fromARGB(255, 87, 17, 17)
+          ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Will?"),
+            Text("Ve no sofa"),
+            Text("Halkins."),
+            Text("23/03/2026")
+          ],
+        )
+        )
       )
     )
   );
