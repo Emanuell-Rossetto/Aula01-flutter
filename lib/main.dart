5d66410026e1ec5945e604a8da54d6905595a3ce
Emanuell-Rossetto <emanuellrossetto@hotmail.com>
2026-03-30 19:52:40 -0300

    Sepadado

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main (){
-  runApp(
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget{
+  
+  const MeuApp({super.key});
+  
+  @override
+  Widget build(BuildContext context){
+    return
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -9,20 +18,20 @@ void main (){
             child: Text("Stranger things"),
           ),
           backgroundColor: const Color.fromARGB(255, 87, 17, 17)
-          ),
+        ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Will?"),
-            Icon(Icons.dangerous),
-            Text("Ve no sofa"),
-            Text("Halkins."),
-            Text("23/03/2026")
-          ],
-        )
+            children: [
+              Text("Will?"),
+              Icon(Icons.dangerous),
+              Text("Ve no sofa"),
+              Text("Halkins."),
+              Text("23/03/2026")
+            ],
+          )
         )
       )
-    )
-  );
+    );
+  } 
 }
