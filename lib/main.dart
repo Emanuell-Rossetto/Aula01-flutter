50aeff9d93c84997085f90633ed903eada490a0a
EMANUELL HENRIQUE ROSSETTO <EHROSSETTO@UNIVEL.UNIVEL.BR>
2026-04-06 20:15:36 -0300

    Adicionando controler e caixa de texto

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 void main (){
   runApp(MeuApp());
@@ -33,7 +36,10 @@ class PaginaInicial extends StatefulWidget{
 class _PaginaInicialState extends State<PaginaInicial>{
 
   int a = 0;
+  String get nome => _textEditingController.text;
   String get texto => a <= 1 ? 'Você procurou o Will no sofa  $a  vez' : 'Você procurou o Will no sofa  $a  vezes';
+  String get texto2 => nome.isEmpty ? 'VOCE NAO DIGITOU SEU NOME AINDA' : 'Olá ${_textEditingController.text}, voce procurou o Will no sofa $a vezes';
+  final TextEditingController _textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context){
@@ -41,16 +47,25 @@ class _PaginaInicialState extends State<PaginaInicial>{
       child: Column( 
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Text(texto2),
           Text(texto),
+          TextField(
+            controller: _textEditingController,
+            decoration: InputDecoration(
+              labelText: 'Digite seu nome',
+            ),
+          ),
           ElevatedButton(
             child: Text('Procurar Will no sofa'),
             onPressed: () { 
               setState(() {
                 a++;
               });
-              
             } ,
-          )
+          ),
+          ElevatedButton(onPressed: (){
+            
+          }, child: Text("Ação")),
         ],
       ),
     );
